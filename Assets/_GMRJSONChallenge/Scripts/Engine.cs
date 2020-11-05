4c4fbbc1c94d5abeebd6d185a447c1c4b96bc75d
Esteban Gaete <zoraskiller@gmail.com>
2020-11-05 01:03:15 -0300

    Added required functionalities. JSON file changes are detected automatically.

---
+++ Assets/_GMRJSONChallenge/Scripts/Engine.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Engine : MonoBehaviour
+{
+	[Header("Elements")]
+	[SerializeField] private PanelView panelView;
+	[SerializeField] private Button closeButton;
+
+	[Header("Configuration")]
+	[SerializeField] private string fileNameAndPath;
+
+	private FileSystemWatcher fileSystemWatcher;
+	private bool hasFileChanged;
+	private Coroutine loadJSONCoroutine;
+
+	private void Start()
+	{
+		fileSystemWatcher = null;
+		hasFileChanged = false;
+		loadJSONCoroutine = null;
+
+		closeButton.onClick.AddListener(CloseApplicationHandler);
+
+		panelView.CleanPanel();
+
+		WatchJSONFile();
+		StartLoadJSON();
+	}
+
+	private void WatchJSONFile()
+	{
+		fileSystemWatcher = new FileSystemWatcher();
+		fileSystemWatcher.Path = Application.streamingAssetsPath;
+		fileSystemWatcher.Filter = fileNameAndPath;
+		fileSystemWatcher.Created += NotifyFileUpdatedHandler;
+		fileSystemWatcher.Deleted += NotifyFileUpdatedHandler;
+		fileSystemWatcher.Changed += NotifyFileUpdatedHandler;
+		fileSystemWatcher.Renamed += NotifyFileUpdatedHandler;
+
+		fileSystemWatcher.EnableRaisingEvents = true;
+	}
+
+	private void Update()
+	{
+		if (!hasFileChanged)
+			return;
+
+		hasFileChanged = false;
+
+		StopCoroutine(loadJSONCoroutine);
+		StartLoadJSON();
+	}
+
+	private void StartLoadJSON()
+	{
+		loadJSONCoroutine = StartCoroutine(LoadJSON());
+	}
+
+	private IEnumerator LoadJSON()
+	{
+		using (var unityWebRequest = UnityWebRequest.Get("file://" + Path.Combine(Application.streamingAssetsPath, fileNameAndPath)))
+		{
+			yield return unityWebRequest.SendWebRequest();
+
+			PanelData panelData;
+
+			if (unityWebRequest.isNetworkError || unityWebRequest.isHttpError)
+			{
+				Debug.Log(unityWebRequest.error);
+				panelData = new PanelData("ERROR: File Not Found", new List<string>());
+			}
+			else
+			{
+				try
+				{
+					panelData = PanelData.ParseJSON(unityWebRequest.downloadHandler.text);
+				}
+				catch (Exception exception)
+				{
+					Debug.Log(exception.Message);
+					panelData = new PanelData("ERROR: Wrong JSON Format", new List<string>());
+				}
+			}
+
+			panelView.UpdatePanelContent(panelData);
+		}
+	}
+
+	private void NotifyFileUpdatedHandler(object sender, FileSystemEventArgs eventArgs)
+	{
+		hasFileChanged = true;
+	}
+
+	private void CloseApplicationHandler()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+}
---
+++ Assets/_GMRJSONChallenge/Scripts/PanelView.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelView : MonoBehaviour
+{
+	[Header("Elements")]
+	[SerializeField] private Text panelTitle;
+	[SerializeField] private GameObject contentRoot;
+
+	[Header("Prefabs")]
+	[SerializeField] private GameObject columnPrefab;
+	[SerializeField] private Text columnTitlePrefab;
+	[SerializeField] private Text columnContentPrefab;
+
+	public void CleanPanel()
+	{
+		panelTitle.text = string.Empty;
+
+		foreach (Transform childTransform in contentRoot.transform)
+		{
+			Destroy(childTransform.gameObject);
+		}
+	}
+
+	public void UpdatePanelContent(PanelData panelData)
+	{
+		CleanPanel();
+
+		panelTitle.text = panelData.Title;
+
+		foreach (var panelDataColumnHeader in panelData.ColumnHeaders)
+		{
+			var column = Instantiate(columnPrefab, contentRoot.transform, false);
+
+			var columnTitle = Instantiate(columnTitlePrefab, column.transform, false);
+			columnTitle.text = panelDataColumnHeader;
+
+			foreach (var contentData in panelData.ContentsData)
+			{
+				var columnContent = Instantiate(columnContentPrefab, column.transform, false);
+				columnContent.text = contentData[panelDataColumnHeader];
+			}
+		}
+	}
+}
